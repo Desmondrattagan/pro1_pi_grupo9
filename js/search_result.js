6c8823cfb8dd5fdfc5665d8f3ed5370fe1b697a2
Desmondrattagan <drattagan@udesa.edu.ar>
2022-11-18 16:28:52 -0300

    Search results

--- js/search_result.js
+++ js/search_result.js
@@ -21,4 +21,45 @@ let objqs = new URLSearchParams(qs);
 let pelicula = objqs.get('buscador')
 let titulo = document.querySelector(".titulosearch")
 titulo.innerText = "Results for: " + pelicula
+let api_key = 'bc6a66de00e3debea99fdcf92ffc0ab7';
+
+let urlp =  `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&language=en-US&page=1&include_adult=false`
+
+fetch(urlp)
+.then(function(response) {
+    return response.json();
+}
+).then(function(data){
+    console.log(data)
+    return data;
+}
+).catch(function(error){
+    return error;
+
+}
+);
+
+
+
+
+
+
+
+let urls = 
+
+fetch(urls)
+.then(function(response) {
+    return response.json();
+}
+).then(function(data){
+    console.log(data)
+    return data;
+}
+).catch(function(error){
+    return error;
+
+}
+);
+
+
 
